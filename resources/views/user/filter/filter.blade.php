f2440c2bf1ea5c1084863efa21f4704f2f3dedf6
MichielGuilliams <michiel1998@gmail.com>
2018-10-04 14:30:58 +0200

    add checkbox

--- resources/views/user/filter/filter.blade.php
+++ resources/views/user/filter/filter.blade.php
@@ -2,6 +2,40 @@
 
 @section('content')
 
+    <table class="filterTable">
+        {{Form::open(array('action' => 'FilterController@getFilteredTraveller', 'method' => 'post'))}}
+        <tr>
+            <td>{{ Form::label('lastname', 'Naam', ['class' => 'field']) }}</td>
+            <td>{{Form::checkbox('lastname', 'lastname')}}</td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('firstname', 'Voornaam', ['class' => 'field']) }}</td>
+            <td>{{Form::checkbox('firstname', 'firstname')}}</td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('email', 'Email', ['class' => 'field']) }}</td>
+            <td>{{Form::checkbox('email', 'email')}}</td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('phone', 'Telefoon', ['class' => 'field']) }}</td>
+            <td>{{Form::checkbox('phone', 'phone')}}</td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('trip_name', 'Reis', ['class' => 'field']) }}</td>
+            <td>{{Form::checkbox('trip_name', 'trip_name')}}</td>
+        </tr>
+        <tr>
+            <td>{{ Form::label('major_name', 'Klas', ['class' => 'field']) }}</td>
+            <td>{{Form::checkbox('major_name', 'major_name')}}</td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                {{Form::submit('Toon Lijst',['class'=>'button'])}}
+            </td>
+        </tr>
+        {{ Form::close() }}
+    </table>
+
     @foreach ($aUserData as $oUserData)
         {{ $oUserData->traveller_id }}
     @endforeach
