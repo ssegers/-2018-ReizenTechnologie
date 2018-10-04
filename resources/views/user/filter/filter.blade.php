e4953ab7ec5b1debf5fbac7a25f623580ff3ab15
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-10-04 14:12:55 +0200

    Layout fix

--- resources/views/user/filter/filter.blade.php
+++ resources/views/user/filter/filter.blade.php
@@ -1,21 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-        @foreach ($aUserData as $oUserData)
-            {{ $oUserData->traveller_id }}
-        @endforeach
-    {{ $aUserData->links() }}
-@endsection
 
-=======
+    @foreach ($aUserData as $oUserData)
+        {{ $oUserData->traveller_id }}
+    @endforeach
+
+    {{ $aUserData->links() }}
+
+@endsection
 @section('style')
     <style>
 
     </style>
->>>>>>> 8f65801c78637ac4d4b4e1fc2d6d1133ee6fdaa3
-@endsection
-
-@section('content')
-
 @endsection
 
