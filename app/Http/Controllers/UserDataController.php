81de0f3086c091ca8851277eb9f52a90cf489891
sashavdv <sashavandevoorde@gmail.com>
2018-10-04 14:33:18 +0200

    added excel download function based on sessions

--- app/Http/Controllers/UserDataController.php
+++ app/Http/Controllers/UserDataController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\UserData;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class UserDataController extends Controller
 {
-    //
+    
+    /*
+     * downloadExcel : this will download an excel file based on the session data of filters (the checked fields)
+     */
+    public function downloadExcel(Request $request)
+    {
+        $aUserFields = $request->session()->get('filters');
+        $data = UserData::get($aUserFields)->toArray();
+
+        return Excel::create('Gebruikers', function($excel) use ($data) {
+            $excel->sheet('mySheet', function($sheet) use ($data)
+            {
+                $sheet->fromArray($data);
+            });
+        })->download('xlsx');
+    }
 }
