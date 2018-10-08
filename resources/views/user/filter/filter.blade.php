0ac950ec2320dae60ef81046dded4e9dc1503af9
Joren Meynen <joren.meynen@telenet.be>
2018-10-08 15:37:47 +0200

    Revert "user.filter: first test with grids"
    This reverts commit db1e69d5a06d3f48c5a859c5afba235863c6bf15.

--- resources/views/user/filter/filter.blade.php
+++ resources/views/user/filter/filter.blade.php
@@ -2,70 +2,31 @@
 
 @section('content')
 
+    {{Form::open(array('action' => 'UserDataController@showUsersAsMentor', 'method' => 'post'))}}
+        <ul>
+            <li>
+                {{ Form::label('email', 'Email', ['class' => 'field']) }}
+                {{ Form::checkbox('email', 'email') }}
+            </li>
+            <li>
+                {{ Form::label('phone', 'Telefoon', ['class' => 'field']) }}
+                {{ Form::checkbox('phone', 'phone') }}
+            </li>
+        </ul>
+        <button type="submit" name="button-filter" value="button-filter">Filter lijst</button>
+    {{ Form::close() }}
 
 
+    @foreach ($aUserData as $oUserData)
+        {{ $oUserData->traveller_id }}
+    @endforeach
 
-    <div class="container">
-        <div class="form_container">
-            <div class="options_container">
-                {{Form::open(array('action' => 'UserDataController@showUsersAsMentor', 'method' => 'post'))}}
-                <div class="option_cell">
-                    {{ Form::checkbox('email', 'email') }}
-                    {{ Form::label('email', 'Email', ['class' => 'field']) }}
-                </div>
-                <div class="option_cell">
-                    {{ Form::checkbox('phone', 'phone') }}
-                    {{ Form::label('phone', 'Telefoon', ['class' => 'field']) }}
-                </div>
-                <div class="option_button">
-                    <button type="submit" name="button-filter" value="button-filter">Filter lijst</button>
-                </div>
-                {{ Form::close() }}
-            </div>
-
-
-        </div>
-        <div class="table_container">
-            @foreach ($aUserData as $oUserData)
-                {{ $oUserData->traveller_id }}
-            @endforeach
-
-            {{ $aUserData->links() }}
-        </div>
-    </div>
-
-
-
+    {{ $aUserData->links() }}
 
 @endsection
 @section('style')
     <style>
-        .container {
-            display: grid;
-            grid-template-columns: 20% auto;
-            grid-template-rows: 400px;
-            grid-column-gap: 5%;
 
-            background: white;
-            padding: 50px;
-            width: 1000px;
-            margin: 0 auto;
-
-
-        }
-        .form_container{
-            background: #E9F3F8;
-        }
-        .table_container{
-            background: #E9F3F8;
-        }
-        .option_cell{
-            display: grid;
-            grid-template-columns: 30px auto;
-            grid-template-rows: 50px;
-        }
-        .option_button{
-        }
     </style>
 @endsection
 
