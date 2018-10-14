3b1255aea75463cc30c91b792b4289f98d13cbe3
Schelf <Nicolaas.schelfhout@gmail.com>
2018-10-14 14:21:19 +0200

    Update resources/views/user/Form/form.blade.php

--- resources/views/user/Form/form.blade.php
+++ resources/views/user/Form/form.blade.php
@@ -1,39 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-
-@endsection
-@section('style')
     <form id="registerForm" action="">
-
         <h1>Schrijf je hier in:</h1>
-
         <!-- One "tab" for each step in the form: -->
         <div class="tab">Kies je reis:
-            <p><select name="reizen">
+            <p><select id="dropReis" name="reizen">
+                    <option></option>
+                </select></p>
+            <p><label>Studentnummer</label><input id="txtStudentnummer" oninput="this.className=''"></p>
+            <p><label>Opleiding</label><select id="dropOpleiding" name="opleidingen">
+                    <option></option>
+                </select></p>
+            <p><label>Afstudeerrichting</label><select id="dropAfstudeerrichting" name="opleidingen">
                     <option></option>
                 </select></p>
         </div>
 
         <div class="tab">Persoonlijke gegevens:
-            <p><<input oninput="this.className = ''"></p>
-            <p><input placeholder="Phone..." oninput="this.className = ''"></p>
+            <p><label>Naam</label><input id="txtNaam" oninput="this.className = ''"></p>
+            <p><label>Voornaam</label><input id="txtVoornaam" oninput="this.className = ''"></p>
+            <p><label>Geslacht</label>
+                <input id="radioGeslacht" type="radio" name="gender" value="male" checked> Man<br>
+                <input id="radioGeslacht"type="radio" name="gender" value="female"> Vrouw<br>
+                <input id="radioGeslacht"type="radio" name="gender" value="other"> Andere</p>
+            <p><label>Nationaliteit</label><input id="txtNationaliteit oninput="this.className = ''"></p>
+            <p><label>Geboortedatum</label><input id="dateGeboorte" type="date" name="bday" oninput="this.className = ''"></p>
+            <p><label>Adres</label><input id="txtAdres" oninput="this.className = ''"></p>
+            <p><label>Land</label><input id="txtLand"oninput="this.className = ''"></p>
+            <p><label>Woonplaats</label><input id="dropGemeente" oninput="this.className = ''"></p>
+            <p><label>Bankrekeningnummer (IBAN)</label><input id="txtBank" oninput="this.className = ''"></p>
+
         </div>
 
         <div class="tab">Contact gegevens:
-            <p><input placeholder="dd" oninput="this.className = ''"></p>
-            <p><input placeholder="mm" oninput="this.className = ''"></p>
-            <p><input placeholder="yyyy" oninput="this.className = ''"></p>
+            <p><label>E-mail adres (van de school)</label><input id="txtEmail" oninput="this.className = ''"></p>
+            <p><label>GSM-nummer</label><input id="txtGsm" oninput="this.className = ''"></p>
+            <p><label>Noodnummer 1</label><input id="txtNoodnummer1" oninput="this.className = ''"></p>
+            <p><label>Noodnummer 2</label><input id="txtNoodnummer2" oninput="this.className = ''"></p>
         </div>
 
         <div class="tab">Medische gegevens:
-            <p><input placeholder="Username..." oninput="this.className = ''"></p>
-            <p><input placeholder="Password..." oninput="this.className = ''"></p>
-        </div>
-
-        <div class="tab">Confirmatie:
-            <p><input placeholder="Username..." oninput="this.className = ''"></p>
-            <p><input placeholder="Password..." oninput="this.className = ''"></p>
+            <p><label>Heeft u een operatie gehad in het afgelopen jaar?</label>
+                <input id="radioMedisch" type="radio" name="check" value="yes" >Ja
+                <input id="radioMedisch" type="radio" name="check" value="no" checked>Neen
+            </p>
+            <p><label>Wat houden de medische aandoeningen in?</label><input id="txtMedisch" oninput="this.className = ''"></p>
         </div>
 
         <div style="overflow:auto;">
@@ -49,8 +61,6 @@
             <span class="step"></span>
             <span class="step"></span>
             <span class="step"></span>
-            <span class="step"></span>
-
         </div>
 
     </form>
