1f54c837d0ccc73a05ebd410700b1515c0c35ad0
StefKerkhofs <kerkhofsstef@hotmail.com>
2018-10-21 14:56:59 +0200

    added more travellers

--- database/seeds/TravellersTableSeeder.php
+++ database/seeds/TravellersTableSeeder.php
@@ -264,5 +264,455 @@ class TravellersTableSeeder extends Seeder
             'medical_issue' => false,
             'medical_info' => null
         ]);
+
+        // Jan Modaal
+        DB::table('travellers')->insert([
+            'user_id' => 13,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Jan',
+            'last_name' => "Modaal",
+            'email' => 'jan.modaal@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'jan zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Piet Janssen
+        DB::table('travellers')->insert([
+            'user_id' => 14,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Piet',
+            'last_name' => "Janssen",
+            'email' => 'piet.janssen@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'piet zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Toon Peeters
+        DB::table('travellers')->insert([
+            'user_id' => 15,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Toon',
+            'last_name' => "Peeters",
+            'email' => 'toon.peters@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'toon zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Gert Nullens
+        DB::table('travellers')->insert([
+            'user_id' => 16,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Gert',
+            'last_name' => "Nullens",
+            'email' => 'gert.nullens@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'gert zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Bram Bongers
+        DB::table('travellers')->insert([
+            'user_id' => 17,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Bram',
+            'last_name' => "Bongers",
+            'email' => 'bram.bongers@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'bram zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Tom Moons
+        DB::table('travellers')->insert([
+            'user_id' => 18,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Tom',
+            'last_name' => "Moons",
+            'email' => 'tom.moons@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'tom zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Jens Janssen
+        DB::table('travellers')->insert([
+            'user_id' => 19,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Jens',
+            'last_name' => "Janssen",
+            'email' => 'jens.janssen@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'jens zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Martijn Theunissen
+        DB::table('travellers')->insert([
+            'user_id' => 20,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Martijn',
+            'last_name' => "Theunissen",
+            'email' => 'Martijn.Theunissen@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'martijn zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Steve Stevens
+        DB::table('travellers')->insert([
+            'user_id' => 21,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Steve',
+            'last_name' => "Stevens",
+            'email' => 'steve.stevens@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'steve zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Dario Thielens
+        DB::table('travellers')->insert([
+            'user_id' => 22,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Dario',
+            'last_name' => "Thielens",
+            'email' => 'dario.thielens@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'dario zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+        //Bert Bertens
+        DB::table('travellers')->insert([
+            'user_id' => 23,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Bert',
+            'last_name' => "Bertens",
+            'email' => 'bert.bertens@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'bert zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+
+        //Piet Pieters
+        DB::table('travellers')->insert([
+            'user_id' => 24,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Piet',
+            'last_name' => "Pieters",
+            'email' => 'piet.pieters@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'piet zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+
+        //Rudy Verboven
+        DB::table('travellers')->insert([
+            'user_id' => 25,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Rudy',
+            'last_name' => "Verboven",
+            'email' => 'rudy.verboven@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'rudy zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+
+
+        //Johnny Bravo
+        DB::table('travellers')->insert([
+            'user_id' => 26,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Johnny',
+            'last_name' => "Bravo",
+            'email' => 'johnny.bravo@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'johnny zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Bjorn Mertens
+        DB::table('travellers')->insert([
+            'user_id' => 27,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Bjorn',
+            'last_name' => "Mertens",
+            'email' => 'bjorn.mertens@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'bjorn zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Jan Tomassen
+        DB::table('travellers')->insert([
+            'user_id' => 28,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Jan',
+            'last_name' => "Tomassen",
+            'email' => 'jan.tomassen@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'jan zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Vincent Ramaekers
+        DB::table('travellers')->insert([
+            'user_id' => 29,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Vincent',
+            'last_name' => "Ramaekers",
+            'email' => 'vincent.ramaekers@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'vincent zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Glenn Vanaken
+        DB::table('travellers')->insert([
+            'user_id' => 30,
+            'trip_id' => 2,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Glenn',
+            'last_name' => "Vanaken",
+            'email' => 'glenn.vanaken@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'glenn zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
+
+        //Roel Aegten
+        DB::table('travellers')->insert([
+            'user_id' => 31,
+            'trip_id' => 1,
+            'zip_id' =>2,
+            'major_id' =>1,
+            'first_name' => 'Roel',
+            'last_name' => "Aegten",
+            'email' => 'roel.aegten@student.ucll.be',
+            'country' => 'belgië',
+            'address' => 'roel zijn straat 15',
+            'gender' => 'man',
+            'phone' => '0474567892',
+            'emergency_phone_1' => '0471852963',
+            'emergency_phone_2' => '0471717171',
+            'nationality' => 'belg',
+            'birthdate' => '01/01/1995',
+            'birthplace' => 'Diest',
+            'iban' => 'BE001111222233334',
+            'medical_issue' => false,
+            'medical_info' => null
+        ]);
     }
 }
