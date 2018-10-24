982cb4cdd042df31657f3e9a5752577125c26eb9
daanvandebosch <daan.vandebosch@hotmail.com>
2018-10-24 17:40:02 +0200

    RegisterController

---
+++ resources/views/mails/registermail.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <p><b>Info verzender:</b></p>
+    <ul>
+        <li><b>Naam:</b>{{ $firstName }} {{ $lastName }}</li>
+        <li><b>Email:</b> {{ $email }}</li>
+        <li><b>password:</b> {{ $password }}</li>
+    </ul>
+</div>
+<div>
+    <p><b>Berricht:</b></p>
+    <p>{{ $description }}</p>
+</div>
