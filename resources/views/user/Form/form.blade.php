b16b4136bca5d757daaec246069384a2fc2b1855
Schelf <Nicolaas.schelfhout@gmail.com>
2018-10-24 17:06:26 +0200

    Form + form.js aanpassing

--- resources/views/user/Form/form.blade.php
+++ resources/views/user/Form/form.blade.php
@@ -7,6 +7,7 @@
         <div class="tab"><h2 class="tabTitle">Basisgegevens:</h2>
             <p><label class="formLabel">Reis:</label>
                 {{ Form::select('dropReis', array('1' => 'USA 2019', '2' => 'Duitsland 2019'), ['id'=>'dropReis','oninput'=>'this.className'])}}</p>
+
             <p><label class="formLabel">Studentnummer</label>{{ Form::text('txtStudentnummer', '', ['id'=>'txtStudentnummer','oninput'=>'this.className'])}}</p>
             <p><label class="formLabel">Opleiding</label>
                 {{ Form::select('dropReis', array('1' => 'ELO-ICT', '2' => 'Docent'), ['id'=>'dropOpleiding','oninput'=>'this.className'])}}</p>
@@ -26,8 +27,9 @@
             <p><label class="formLabel">Geboortedatum</label>{{ Form::date('dateGeboorte', '', ['id'=>'dateGeboorte','oninput'=>'this.className','type'=>'date'])}}</p>
             <p><label class="formLabel">Geboorteplaats</label>{{ Form::text('txtGeboorteplaats', '', ['id'=>'txtGeboorteplaats','oninput'=>'this.className'])}}</p>
             <p><label class="formLabel">Adres</label>{{ Form::text('txtAdres', '', ['id'=>'txtAdres','oninput'=>'this.className'])}}</p>
-            <p><label class="formLabel">Land</label>{{ Form::text('txtLand', '', ['id'=>'txtLand','oninput'=>'this.className'])}}</p>
+
             <p><label class="formLabel">Woonplaats</label>{{ Form::text('dropGemeente', '', ['id'=>'dropGemeente','oninput'=>'this.className'])}}</p>
+            <p><label class="formLabel">Land</label>{{ Form::text('txtLand', '', ['id'=>'txtLand','oninput'=>'this.className'])}}</p>
             <p><label class="formLabel">Bankrekeningnummer (IBAN)</label>{{ Form::text('txtBank', '', ['id'=>'txtBank','oninput'=>'this.className'])}}</p>
 
         </div>
@@ -36,7 +38,7 @@
             <p><label class="formLabel">E-mail adres (van de school)</label><input id="txtEmail" oninput="this.className = ''"></p>
             <p><label class="formLabel">GSM-nummer</label>{{ Form::text('txtGsm', '', ['id'=>'txtGsm','oninput'=>'this.className'])}}</p>
             <p><label class="formLabel">Noodnummer 1</label>{{ Form::text('txtNoodnummer1', '', ['id'=>'txtNoodnummer1','oninput'=>'this.className'])}}</p>
-            <p><label class="formLabel">Noodnummer 2</label>{{ Form::text('txtNoodnummer2', '', ['id'=>'txtNoodnummer2','oninput'=>'this.className'])}}</p>
+            <p><label class="formLabel">Noodnummer 2</label>{{ Form::text('txtNoodnummer2', '', ['id'=>'txtNoodnummer2','oninput'=>'this.className', 'placeholder'=>'Niet verplicht'])}}</p>
         </div>
 
     <div class="tab"><h2 class="tabTitle">Medische gegevens:</h2>
@@ -45,7 +47,7 @@
                 {{ Form::radio('check', 'no', ['id'=>'radioMedisch','oninput'=>'this.className'])}}Nee
 
             </p>
-            <p><label class="formLabel">Wat houden deze in?</label>{{ Form::textarea('txtMedisch', '', ['id'=>'txtMedisch','oninput'=>'this.className'])}}</p>
+        <p><label class="formLabel">Wat houden deze in?</label></p><br>{{ Form::textarea('txtMedisch', '', ['id'=>'txtMedisch','oninput'=>'this.className','placeholder'=>'Niet verplicht'])}}
         </div>
 
         <div style="overflow:auto;">
