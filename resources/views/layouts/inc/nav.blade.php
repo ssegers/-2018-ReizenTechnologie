e06eba14d5a9297a494b4c47958b416b947e36d4
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-10-25 09:59:07 +0200

    Update resources/views/layouts/inc/nav.blade.php

--- resources/views/layouts/inc/nav.blade.php
+++ resources/views/layouts/inc/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="{{ route('info') }}">
         <img src="{{ asset('images/ucll.png') }}" alt="">
     </a>
