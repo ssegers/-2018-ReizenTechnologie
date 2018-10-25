2002b84234414dbc55d5659a3daa9a1ff2f385fb
daanvandebosch <daan.vandebosch@hotmail.com>
2018-10-25 11:32:07 +0200

    RegisterController

--- resources/views/user/KiesOrganisator.blade.php
+++ resources/views/user/KiesOrganisator.blade.php
@@ -6,7 +6,28 @@
         <div class="row">
             <div class="col"></div>
             <div class="col">
-                <label>Kies uw reis</label>
+                <br>
+                <label>Kies uw reis: </label>
+                <select name="ReisKiezen" class="select">
+                    <?php
+                    $aAllTrips = \App\Trip::all()->where('is_active','<>','true');
+                    echo $aAllTrips;
+                    foreach ($aAllTrips as $oTrip){
+                    ?>
+                    <option value="<?php echo $oTrip->trip_id ?>"><?php echo $oTrip->name ?></option>
+                <?php } ?>
+                    </select>
+                <br>
+                <label>Kies uw reis: </label>
+                <select name="OrganisatorKiezen" class="select">
+                    <?php
+                    $aAllTrips = \App\Trip::all()->where('is_active','<>','true');
+                    echo $aAllTrips;
+                    foreach ($aAllTrips as $oTrip){
+                    ?>
+                    <option value="<?php echo $oTrip->trip_id ?>"><?php echo $oTrip->name ?></option>
+                    <?php } ?>
+                </select>
 
             </div>
             <div class="col"></div>
