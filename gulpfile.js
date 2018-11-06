4d10574800217ff0145cb0a973046d79a7d02890
daanvandebosch <daan.vandebosch@hotmail.com>
2018-11-06 10:34:10 +0100

    RegisterController

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss');
     mix.scripts('activeTripOrganiser.js');
-    mix.scripts(['form.js','iban.js','dropswitch.js','jquery-3.3.1.js']);
+    mix.scripts(['form.js','iban.js']);
+    mix.scripts(['dropswitch.js']);
+    mix.scripts(['jquery-3.3.1.js']);
 });
