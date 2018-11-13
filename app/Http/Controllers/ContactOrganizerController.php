9b775383cf42d4a7d702ea04ec5c7d721e5fd0fe
akpik <kaan.akpinar@hotmail.com>
2018-11-13 19:35:44 +0100

    nav voor admin contact en data query

--- app/Http/Controllers/ContactOrganizerController.php
+++ app/Http/Controllers/ContactOrganizerController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Trip;
 use Illuminate\Http\Request;
 
 class ContactOrganizerController extends Controller
 {
     //
+    function getInfo(){
+        $oTrips = Trip::all();
+        $oOrganizers = User::Where('role', 'organizer')
+            ->join('travellers','travellers.user_id','=','users.user_id')->select('first_name', 'last_name', 'traveller_id','email')->get();
+
+        return view( 'admin.contact.contact',
+            [
+                'aTrips' => $oTrips,
+                'aOrganizers' => $oOrganizers,
+            ]);
+    }
 }
