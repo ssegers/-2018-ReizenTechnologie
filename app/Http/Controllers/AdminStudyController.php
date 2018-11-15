665d52efd9360c8b6db3950af7bbc1a4146020e6
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-11-15 14:04:33 +0100

    Studies

--- app/Http/Controllers/AdminStudyController.php
+++ app/Http/Controllers/AdminStudyController.php
@@ -11,22 +11,29 @@ use App\Http\Controllers\Controller;
 
 class AdminStudyController extends Controller {
     /**
-     * This function returns a view with all data from majors and studies
+     * This function shows the view
      *
      * @author Yoeri op't Roodt
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index() {
-//        $aMajors = Major::get();
-        $aStudies = Study::get();
-        $iStudyCount = Study::get()->count();
+        return view('admin.studies.studies');
+    }
 
-        return view('admin.studies.studies', array(
-//            'aMajors' => $aMajors,
+    /**
+     * This method returns the list of studies
+     *
+     * @author Yoeri op't Roodt
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getStudies() {
+        $aStudies = Study::get();
+
+        return response()->json([
             'aStudies' => $aStudies,
-            'iStudyCount' => $iStudyCount,
-        ));
+        ]);
     }
 
     /**
