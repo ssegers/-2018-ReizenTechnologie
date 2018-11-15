e03c66514af3742869ec5396f4fd6f95de36f872
MichielGuilliams <michiel1998@gmail.com>
2018-11-15 15:32:58 +0100

    Update pdfpage.blade.php

--- resources/views/guest/pdfpage.blade.php
+++ resources/views/guest/pdfpage.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="content-center">
-        <embed style="margin: auto; display: block;" src="/storage/upload/{{$aPdfPages->content}}" type="application/pdf" width="90%" class="responsive" height="600px" />
+        <embed class="embed-responsive-item" id="preview" src="{{$aPdfPages->content}}" type='application/pdf'>
     </div>
 @endsection
