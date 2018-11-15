706a025691874d4fd3589488a54b7be3d258a198
akpik <kaan.akpinar@hotmail.com>
2018-11-15 09:54:19 +0100

    contactcontroller geupdate zodat deze alles wat nodig is naar blade verstuurt

--- app/Http/Controllers/ContactPageController.php
+++ app/Http/Controllers/ContactPageController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Trip;
 use Illuminate\Http\Request;
 
 class ContactPageController extends Controller
 {
     //
+    public function getInfo(){
+        $oActiveTrips = Trip::where('is_active',true);
+        return view('guest.contactpage',array('oActiveTrips'=>$oActiveTrips));
+    }
+    public function sendMail(){
+        
+    }
+
 }
