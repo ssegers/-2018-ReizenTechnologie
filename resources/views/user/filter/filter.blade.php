9975e725b4ebb359ed68cbcfbf86a829f0963e15
akpik <kaan.akpinar@hotmail.com>
2018-11-21 19:48:49 +0100

    filter tabel hoverable

--- resources/views/user/filter/filter.blade.php
+++ resources/views/user/filter/filter.blade.php
@@ -39,7 +39,7 @@
         </ul>
     </div>
             <div class="table-wrapper-scroll">
-                <table class="table table-striped">
+                <table class="table table-striped table-hover">
                     <thead>
                     <tr>
                         @foreach($aFiltersChecked as $sFilterValue)
