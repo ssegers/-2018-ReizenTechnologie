90a64b83b30cf6bdff8752b3fc292f66588ab7a5
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-11-21 19:04:57 +0100

    Styling Study-Major

--- resources/views/admin/studies/studies.blade.php
+++ resources/views/admin/studies/studies.blade.php
@@ -5,21 +5,47 @@
     @csrf
     <div id="messages"></div>
     <div class="row">
-        <div class="col-xs-6">
-            <select id="studySelect"></select>
-            <div>
-                <input id="studyInput" type="text">
-                <button id="studyAdd">Add</button>
-            </div>
+        <div class="col-sm">
+            <h2>Richtingen</h2>
         </div>
-        <div class="col-xs-6">
-            <ul id="majorList"></ul>
-            <div>
-                <input id="majorInput" type="text">
-                <button id="majorAdd">Add</button>
-            </div>
+        <div class="col-sm">
+            <h2>Afstudeerrichtingen</h2>
         </div>
     </div>
+    <div class="row">
+        <div class="col-sm">
+            <select class="form-control" id="studySelect"></select>
+        </div>
+        <div class="col-sm">
+            <ul id="majorList"></ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm">
+            <label for="studyInput">Niewe richting toevoegen</label>
+            <input class="form-control" id="studyInput" type="text">
+            <button class="form-control" id="studyAdd">Voeg toe</button>
+        </div>
+        <div class="col-sm">
+            <label for="majorInput">Niewe afstudeerrichting toevoegen</label>
+            <input class="form-control" id="majorInput" type="text">
+            <button class="form-control" id="majorAdd">Voeg toe</button>
+        </div>
+    </div>
+
+    <style>
+        #studySelect {
+            /*margin: 2em 0;*/
+        }
+        #majorList {
+            background-color: white;
+            /*height: calc(100% - 4em);*/
+            height: 100%;
+            border: 1px solid lightgrey;
+            border-radius: 4px;
+            /*margin: 2em 0;*/
+        }
+    </style>
 @endsection
 
 @section('scripts')
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -11,6 +11,7 @@
 
     <title>{{config('app.name')}}</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
