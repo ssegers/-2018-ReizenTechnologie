88f633f6fca1224335f96e38c3459946acbdeb6c
sashavdv <sashavandevoorde@gmail.com>
2018-11-27 13:33:20 +0100

    Added relations to major and traveller

--- app/Traveller.php
+++ app/Traveller.php
@@ -14,14 +14,14 @@ class Traveller extends Model
         return $this->belongsTo('App\User');
     }
 
-    public function trip()
-    {
-        return $this->belongsTo('App\Trip');
-    }
-
     public function zip()
     {
         return $this->belongsTo('App\Zip');
+    }
+
+    public function major()
+    {
+        return $this->belongsTo('App\Major');
     }
 
     public $timestamps = false;
