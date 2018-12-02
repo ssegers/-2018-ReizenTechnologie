2fcf92369affb380472dd0b749707a01e99665b0
Schelf <Nicolaas.schelfhout@gmail.com>
2018-12-02 19:22:21 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,8 @@ Route::prefix('user')->group(function () {
         route::post('step-3', 'RegisterController@step3Post');
 
     });
-
+    Route::get('payment', 'PaymentsOverviewController@showTable')->name('paymentOverview');
+    Route::get('payment', 'PaymentsOverviewController@showTable');
     /* Update Mail as Organizer */
     Route::get('updatemail','MailController@getUpdateForm')->name('updatemail');
     Route::post('updatemail', 'MailController@sendUpdateMail');
