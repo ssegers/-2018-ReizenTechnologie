37bfcb50aeffb1b725a806ffbcd661e7244da745
daanvandebosch <daan.vandebosch@hotmail.com>
2018-12-06 13:40:36 +0100

    fix userinfo bug

--- app/User.php
+++ app/User.php
@@ -6,7 +6,6 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\CanResetPassword;
-
 class User extends Authenticatable
 {
     protected $guarded = ['user_id'];
