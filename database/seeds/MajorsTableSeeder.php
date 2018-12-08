3becc72b6b9e350d4babda240f226600b252678f
Joren Meynen <joren.meynen@telenet.be>
2018-12-08 22:13:07 +0100

    updated majors table

--- database/seeds/MajorsTableSeeder.php
+++ database/seeds/MajorsTableSeeder.php
@@ -12,36 +12,39 @@ class MajorsTableSeeder extends Seeder
      */
     public function run()
     {
+        //id = 1
         DB::table('majors')->insert(array(
             'study_id' => 1,
             'major_name' => "ICT"
         ));
+        //id = 2
         DB::table('majors')->insert(array(
 
             'study_id' => 1,
             'major_name' => "ELO"
         ));
-
+        //id = 3
         DB::table('majors')->insert(array(
 
             'study_id' => 2,
             'major_name' => "EM"
         ));
+        //id = 4
         DB::table('majors')->insert(array(
             'study_id' => 2,
             'major_name' => "ENT"
         ));
 
+        /**
+         * IDs hier hetzelfde laten aub
+         * Deze worden gebruikt om de rol van de user te bepalen
+         */
+        //id = 5
         DB::table('majors')->insert(array(
             'study_id' => 3,
-            'major_name' => "Begeleider"
+            'major_name' => "Docent"
         ));
-
-        DB::table('majors')->insert(array(
-            'study_id' => 3,
-            'major_name' => "Organisator"
-        ));
-
+        //id = 6
         DB::table('majors')->insert(array(
             'study_id' => 3,
             'major_name' => "Extern"
