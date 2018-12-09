d5863159f78bb883f51584f3a2eb2de5ae8eb61c
Joren Meynen <joren.meynen@telenet.be>
2018-12-09 22:07:03 +0100

    Try custom validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
+use App\Traveller;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
-use Validator;
 use Intervention\Validation\ValidatorExtension;
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
         Validator::resolver(function($translator, $data, $rules, $messages)
         {
             return new ValidatorExtension($translator, $data, $rules, $messages, []);
+        });
+
+        Validator::extend('uniqueEmailAndExtension', function ($attribute, $value, $parameters, $validator) {
+            $sFullEmail = $value . '@' . $parameters[0]; //$value should be first part of the email, $parameters[0] should be the extension;
+            $count = Traveller::where('email', $sFullEmail)->count();
+            return $count === 0;    //if false, throw error
         });
     }
 
