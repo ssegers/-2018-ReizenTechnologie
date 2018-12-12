62b2674f8daf6a9cdcddffbb470db283cf1d3d38
Joren Meynen <joren.meynen@telenet.be>
2018-12-12 11:10:39 +0100

    fix typfout

--- resources/views/user/profile/profile.blade.php
+++ resources/views/user/profile/profile.blade.php
@@ -35,7 +35,7 @@
                                         <div class="col-7">{{ $aUserData['gender'] }}</div>
                                     </div>
                                     <div class="form-row">
-                                        <div class="col-5">Klas</div>
+                                        <div class="col-5">Richting</div>
                                         <div class="col-7">{{ $aUserData['study_name'] }}</div>
                                     </div>
                                     <div class="form-row">
