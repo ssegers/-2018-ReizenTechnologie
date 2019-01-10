92f8cf83563c69dccd471ba8e378365cb7b1a4c0
Joren Meynen <joren.meynen@telenet.be>
2019-01-10 22:51:47 +0100

    titel postcode toevoegen in registratie formulier

--- resources/views/user/Form/step2.blade.php
+++ resources/views/user/Form/step2.blade.php
@@ -126,7 +126,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Organisators</h5>
+                    <h5 class="modal-title">Postcode toevoegen</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
