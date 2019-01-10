78b0d25fade590221e802e734e57e57276cf1b3d
daanvandebosch <daan.vandebosch@hotmail.com>
2019-01-10 13:55:49 +0100

    reset password v3

--- resources/views/mails/resetpasswordmail.blade.php
+++ resources/views/mails/resetpasswordmail.blade.php
@@ -1,5 +1,5 @@
 <div>
     <p><b>Beste {{ $name }}, je kan je passwoord aanpassen via:
-            <a href="<?php echo "http://" . "10.1.1.33" . "/password/resetpassword/"; ?>{{$token}}">Klik hier</a></b></p>
+            <a href="<?php echo "http://" . env("DB_HOST") . "/password/resetpassword/"; ?>{{$token}}">Klik hier</a></b></p>
     <p>Pas op deze link is maar 30 minuten geldig</p>
 </div>
